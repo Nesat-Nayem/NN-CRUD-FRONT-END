19efa3490f3ff297955d65f56da50a2211eb5ea6
Nesat Nayem <nesatnayem042@gmail.com>
2022-08-31 10:40:28 +0600

    404 page

--- src/Component/NotFound.jsx
+++ src/Component/NotFound.jsx
@@ -1,9 +1,6 @@
-// import notfound from '../Assets/Images/NotFound.jpg';
-
 const NotFound = () => {
     return (
-        <h1 style={{ margin: '80px 0 0 35%'}}>page not found</h1>
-        // <img src={notfound}  style={{width: '30%', margin: '80px 0 0 35%'}}/>
+        <h1 style={{ margin: '80px 0 0 35%', color:'red', fontWeight:'bold'}}>PAGE NOT FOUND</h1>
     )
 }
 
