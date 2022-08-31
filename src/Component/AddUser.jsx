5c9c8e642de3b613801a6fd01aae702604a40f03
Nesat Nayem <nesatnayem042@gmail.com>
2022-08-31 10:21:19 +0600

    add user consize

--- src/Component/AddUser.jsx
+++ src/Component/AddUser.jsx
@@ -1,16 +1,23 @@
-import react, { useState } from 'react';
-import { FormGroup, FormControl, InputLabel, Input, Button, styled, Typography } from '@mui/material';
-import { addUser } from '../Service/api';
-import { useNavigate } from 'react-router-dom';
-// import swal from 'sweetalert2';
-import Swal from 'sweetalert2';
+import { useState } from "react";
+import {
+  FormGroup,
+  FormControl,
+  InputLabel,
+  Input,
+  Button,
+  styled,
+  Typography,
+} from "@mui/material";
+import { addUser } from "../Service/api";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const initialValue = {
-    name: '',
-    username: '',
-    email: '',
-    phone: ''
-}
+  name: "",
+  username: "",
+  email: "",
+  phone: "",
+};
 
 const Container = styled(FormGroup)`
     width: 50%;
@@ -20,50 +27,72 @@ const Container = styled(FormGroup)`
 `;
 
 const AddUser = () => {
-    const [user, setUser] = useState(initialValue);
-    const { name, username, email, phone } = user;
-    
-    let navigate = useNavigate();
+  const [user, setUser] = useState(initialValue);
+  const { name, username, email, phone } = user;
 
-    const onValueChange = (e) => {
-        setUser({...user, [e.target.name]: e.target.value})
-    }
+  let navigate = useNavigate();
 
-    const addUserDetails = async() => {
-        await addUser(user);
-        navigate('/');
+  const onValueChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-        Swal.fire(
-            'Good job!',
-            'New User Created!',
-            'success'
-          )
-    }
+  const addUserDetails = async () => {
+    await addUser(user);
+    navigate("/");
 
-    return (
-        <Container>
-            <Typography variant="h4">Add User</Typography>
-            <FormControl>
-                <InputLabel htmlFor="my-input">Name</InputLabel>
-                <Input onChange={(e) => onValueChange(e)} name='name' value={name} id="my-input" />
-            </FormControl>
-            <FormControl>
-                <InputLabel htmlFor="my-input">Username</InputLabel>
-                <Input onChange={(e) => onValueChange(e)} name='username' value={username} id="my-input" />
-            </FormControl>
-            <FormControl>
-                <InputLabel htmlFor="my-input">Email</InputLabel>
-                <Input onChange={(e) => onValueChange(e)} name='email' value={email} id="my-input"/>
-            </FormControl>
-            <FormControl>
-                <InputLabel htmlFor="my-input">Phone</InputLabel>
-                <Input onChange={(e) => onValueChange(e)} name='phone' value={phone} id="my-input" />
-            </FormControl>
-            <FormControl>
-                <Button variant="contained" color="primary" onClick={() => addUserDetails()}>Add User</Button>
-            </FormControl>
-        </Container>
-    )
-}
+    Swal.fire("Good job!", "New User Created!", "success");
+  };
+
+  return (
+    <Container>
+      <Typography variant="h4">Add User</Typography>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Name</InputLabel>
+        <Input
+          onChange={(e) => onValueChange(e)}
+          name="name"
+          value={name}
+          id="my-input"
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Username</InputLabel>
+        <Input
+          onChange={(e) => onValueChange(e)}
+          name="username"
+          value={username}
+          id="my-input"
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Email</InputLabel>
+        <Input
+          onChange={(e) => onValueChange(e)}
+          name="email"
+          value={email}
+          id="my-input"
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Phone</InputLabel>
+        <Input
+          onChange={(e) => onValueChange(e)}
+          name="phone"
+          value={phone}
+          id="my-input"
+        />
+      </FormControl>
+      <FormControl>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => addUserDetails()}
+        >
+          Add User
+        </Button>
+      </FormControl>
+    </Container>
+  );
+};
 
 export default AddUser;
