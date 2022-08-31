02a3dfaa879e49a503a14f715839d044b0f2e825
Nesat Nayem <nesatnayem042@gmail.com>
2022-08-31 10:19:24 +0600

    all user pretiar

--- src/Component/AllUsers.jsx
+++ src/Component/AllUsers.jsx
@@ -1,90 +1,95 @@
-import react, { useState, useEffect } from 'react';
-import { Table, TableHead, TableCell, Paper, TableRow, TableBody, Button, styled } from '@mui/material'
-import { getUsers, deleteUser } from '../Service/api';
-import { Link } from 'react-router-dom';
-import Swal from 'sweetalert2';
+import react, { useState, useEffect } from "react";
+import {
+  Table,
+  TableHead,
+  TableCell,
+  TableRow,
+  TableBody,
+  Button,
+  styled,
+} from "@mui/material";
+import { getUsers, deleteUser } from "../Service/api";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const StyledTable = styled(Table)`
-    width: 90%;
-    margin: 50px 0 0 50px;
+  width: 90%;
+  margin: 50px 0 0 50px;
 `;
 
 const THead = styled(TableRow)`
-    & > th {
-        font-size: 20px;
-        background: #000000;
-        color: #FFFFFF;
-    }
+  & > th {
+    font-size: 20px;
+    background: #000000;
+    color: #ffffff;
+  }
 `;
 
 const TRow = styled(TableRow)`
-    & > td{
-        font-size: 18px
-    
-    }
+  & > td {
+    font-size: 18px;
+  }
 `;
 
 const AllUsers = () => {
-    const [users, setUsers] = useState([]);
-    
-    useEffect(() => {
-        getAllUsers();
-    }, []);
+  const [users, setUsers] = useState([]);
 
-    const deleteUserData = async (id) => {
-        await deleteUser(id);
-        getAllUsers();
-        Swal.fire(
-            'Good job!',
-            'Delete User!',
-            'success'
-          )
-    }
+  useEffect(() => {
+    getAllUsers();
+  }, []);
 
-    const getAllUsers = async () => {
-        let response = await getUsers();
-        setUsers(response.data);
-     
-    }
+  const deleteUserData = async (id) => {
+    await deleteUser(id);
+    getAllUsers();
+    Swal.fire("Good job!", "Delete User!", "success");
+  };
 
-    return (
-        <StyledTable>
-            <TableHead>
-                <THead>
-                    <TableCell>Name</TableCell>
-                    <TableCell>Username</TableCell>
-                    <TableCell>Email</TableCell>
-                    <TableCell>Phone</TableCell>
-                    <TableCell style={{marginRight:"-20px"}}> Action </TableCell>
-                </THead>
-            </TableHead>
-            <TableBody>
-                {users.map((user) => (
-                    <TRow key={user.id}>
-                        {/* <TableCell>{user._id}</TableCell> change it to user.id to use JSON Server */}
-                        <TableCell style={{color:'green'}}> 
-                      
-                           
-                          {user.name}
-                     
-                            </TableCell>
-                        <TableCell>{user.username}</TableCell>
-                        <TableCell>{user.email}</TableCell>
-                        <TableCell>{user.phone}</TableCell>
-                        <TableCell>
-                            <Button color="primary" variant="contained" style={{marginRight:10}} component={Link} to={`/edit/${user._id}`}>Edit</Button> {/* change it to user.id to use JSON Server */}
-                            <Button color="secondary" variant="contained" onClick={() => deleteUserData(user._id)}>Delete</Button> {/* change it to user.id to use JSON Server */}
-                        </TableCell>
-                    </TRow>
-                ))}
-            </TableBody>
-        </StyledTable>
+  const getAllUsers = async () => {
+    let response = await getUsers();
+    setUsers(response.data);
+  };
 
-
-
-
-
-    )
-}
+  return (
+    <StyledTable>
+      <TableHead>
+        <THead>
+          <TableCell>Name</TableCell>
+          <TableCell>Username</TableCell>
+          <TableCell>Email</TableCell>
+          <TableCell>Phone</TableCell>
+          <TableCell style={{ marginRight: "-20px" }}> Action </TableCell>
+        </THead>
+      </TableHead>
+      <TableBody>
+        {users.map((user) => (
+          <TRow key={user.id}>
+            <TableCell style={{ color: "green" }}>{user.name}</TableCell>
+            <TableCell>{user.username}</TableCell>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{user.phone}</TableCell>
+            <TableCell>
+              <Button
+                color="primary"
+                variant="contained"
+                style={{ marginRight: 10 }}
+                component={Link}
+                to={`/edit/${user._id}`}
+              >
+                Edit
+              </Button>
+              <Button
+                color="secondary"
+                variant="contained"
+                onClick={() => deleteUserData(user._id)}
+              >
+                Delete
+              </Button>
+            </TableCell>
+          </TRow>
+        ))}
+      </TableBody>
+    </StyledTable>
+  );
+};
 
 export default AllUsers;
