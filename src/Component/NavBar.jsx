0b9c70eb11db2fda101e84a1da97b67225893497
Nesat Nayem <nesatnayem042@gmail.com>
2022-08-31 10:38:36 +0600

    navbar solve

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,64 +1,47 @@
+import { AppBar, Toolbar, styled } from "@mui/material";
 
-import { AppBar, Toolbar, styled } from '@mui/material';
+import { NavLink } from "react-router-dom";
 
-import { NavLink } from 'react-router-dom';
-
-// mui nave bar 
-
-import Box from '@mui/material/Box';
-// import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
 
 const Header = styled(AppBar)`
-    background: #111111;
+  background: #111111;
 `;
-    
+
 const Tabs = styled(NavLink)`
-    color: #FFFFFF;
-    margin-right: 20px;
-    text-decoration: none;
-    font-size: 20px;
-  
+  color: #ffffff;
+  margin-right: 20px;
+  text-decoration: none;
+  font-size: 20px;
 `;
 
 const NavBar = () => {
-    return (
-
-        // mui nave bar 
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              {/* <MenuIcon /> */}
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              NN NODE CRUD APPLICATION
-            </Typography>
-            <Button color="inherit">HOME</Button>
-            <Button color="inherit">Add User</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-        // mui nave bar 
-
-
-        // <Header position="static">
-        //     <Toolbar >
-        //         {/* <Tabs to="./" exact>Code for Interview</Tabs> */}
-        //         <Tabs to="/" exact>All Users</Tabs>
-        //         <Tabs to="add" exact>Add User</Tabs>
-        //     </Toolbar>
-        // </Header>
-    )
-}
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            NN NODE CRUD APPLICATION
+          </Typography>
+          <Tabs to="/" exact>
+            HOME
+          </Tabs>
+          <Tabs to="add" exact>
+            Add User
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
 export default NavBar;
