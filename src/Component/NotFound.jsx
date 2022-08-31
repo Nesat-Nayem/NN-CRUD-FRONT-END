588cc17f1fc6a6a5df16cec47bb2bd493e0091bd
Nesat Nayem <nesatnayem042@gmail.com>
2022-08-31 10:06:03 +0600

    crud component

---
+++ src/Component/NotFound.jsx
@@ -0,0 +1,10 @@
+// import notfound from '../Assets/Images/NotFound.jpg';
+
+const NotFound = () => {
+    return (
+        <h1 style={{ margin: '80px 0 0 35%'}}>page not found</h1>
+        // <img src={notfound}  style={{width: '30%', margin: '80px 0 0 35%'}}/>
+    )
+}
+
+export default NotFound;
